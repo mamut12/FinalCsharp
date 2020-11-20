305767a982f15e2f146da36d614d9b644bc067cd
Noushin <n01464164@humbermail.ca>
2020-11-19 20:08:23 -0500

    final exam

--- FinalCsharp/Program.cs
+++ FinalCsharp/Program.cs
@@ -2,11 +2,46 @@
 
 namespace FinalCsharp
 {
+    interface Person
+    {
+    }
+
+    public class Adult : Person
+    {
+        string firstName;
+        string lastName;
+        public Adult(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+    }
+
+    public class Child : Person
+    {
+        string firstName;
+        string lastName;
+        public Child(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Adult a = new Adult("Noushin", "Towf");
+            Child c = new Child("Nana", "mala");
+
+            Person[] array = { a, c };
+            foreach(Person person in array)
+            {
+
+            }
+
         }
     }
 }
